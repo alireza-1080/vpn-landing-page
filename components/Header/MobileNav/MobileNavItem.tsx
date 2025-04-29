aeb02e1ebe61bba3c0d7760d5d65ee1aae3089d4
Alireza Aberoumand <maya7192@yahoo.com>
2025-04-28 22:53:59 -0700

    Refactor MobileNavItem component to adjust class order for improved styling consistency

--- components/Header/MobileNav/MobileNavItem.tsx
+++ components/Header/MobileNav/MobileNavItem.tsx
@@ -12,7 +12,7 @@ const MobileNavItem = ({
   return (
     <SheetClose asChild>
       <Link href={href}>
-        <span className="cursor-pointer px-5 py-2 font-semibold text-blue-500 sm:text-lg">
+        <span className="cursor-pointer px-5 py-2 text-lg font-semibold text-blue-500">
           {children}
         </span>
       </Link>
